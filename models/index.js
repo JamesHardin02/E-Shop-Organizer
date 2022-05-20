a4ea053d60c4a65828ffc2c5e0399319553093d3
JamesH <jwhusc7@gmail.com>
2022-05-20 09:39:44 -0400

    added associations for Category and Product in models/index.js

--- models/index.js
+++ models/index.js
@@ -4,9 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
